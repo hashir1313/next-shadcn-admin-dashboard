71d8b8758b0219f2ea3d0e388f5a5def8226b305
Muhammad Hashir <muhammadhashirshabbir@proton.me>
2026-07-05 18:11:33 +0500

    favicons added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -33,6 +33,12 @@ export default function RootLayout({ children }: Readonly<{ children: ReactNode 
       suppressHydrationWarning
     >
       <head>
+        <link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        <meta name="apple-mobile-web-app-title" content="Trakki" />
+        <link rel="manifest" href="/site.webmanifest" />
         {/* Applies theme and layout preferences on load to avoid flicker and unnecessary server rerenders. */}
         <ThemeBootScript />
       </head>
